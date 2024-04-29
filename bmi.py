b1e8baa0cbe3487cfb57f48c61cd5e4069a7b2ce
Hong Yi <leehy.23@ichat.sp.edu.sg>
2024-04-29 10:04:34 +0800

    lab3 portion

--- bmi.py
+++ bmi.py
@@ -1,14 +1,19 @@
 def calculate_bmi(height, weight): 
+    result = []
     print("Height = " + str(height)) 
     print("Weight = " + str(weight)) 
+
 #Add code here to calculate BMI 
     bmi=(weight/(height**2))
 #Add code here to display calculate BMI 
     print("BMI =" +str(bmi)) 
     if bmi < 18.5:
         print("User is underweight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("User is normal weight")
+        return 0
     else:
         print("User is overweight")
-calculate_bmi(weight=57, height=1.73)  
+        return 1
+calculate_bmi(weight=57, height=1.73)
